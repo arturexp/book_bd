4e22f80e89d2b68cef80fb8da68b9f91db28b97a
Shaginyan Artur <arturexp@gmail.com>
2021-02-27 22:46:32 +0700

    some prettify changes

--- frontend.py
+++ frontend.py
@@ -54,35 +54,35 @@ window = Tk()
 
 window.wm_title("BookStore")
 
-l1 = Label(window, text='Title')
+l1 = Label(window, text='Title', width=5)
 l1.grid(row=0, column=0)
 
-l2 = Label(window, text='Author')
+l2 = Label(window, text='Author', width=5)
 l2.grid(row=0, column=2)
 
-l3 = Label(window, text='Year')
+l3 = Label(window, text='Year', width=5)
 l3.grid(row=1, column=0)
 
-l4 = Label(window, text='ISBN')
+l4 = Label(window, text='ISBN', width=5)
 l4.grid(row=1, column=2)
 
 title_text = StringVar()
-e1 = Entry(window, textvariable=title_text)
+e1 = Entry(window, textvariable=title_text, width=35)
 e1.grid(row=0, column=1)
 
 author_text = StringVar()
-e2 = Entry(window, textvariable=author_text)
+e2 = Entry(window, textvariable=author_text, width=35)
 e2.grid(row=0, column=3)
 
 year_text = StringVar()
-e3 = Entry(window, textvariable=year_text)
+e3 = Entry(window, textvariable=year_text, width=35)
 e3.grid(row=1, column=1)
 
 isbn_text = StringVar()
-e4 = Entry(window, textvariable=isbn_text)
+e4 = Entry(window, textvariable=isbn_text, width=35)
 e4.grid(row=1, column=3)
 
-list1 = Listbox(window, height=6, width=35)
+list1 = Listbox(window, height=6, width=50, borderwidth=2)
 list1.grid(row=2, column=0, rowspan=6, columnspan=2)
 
 sb1 = Scrollbar(window)
